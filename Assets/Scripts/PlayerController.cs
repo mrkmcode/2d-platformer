1a9ee3b7f75a3b43377be15f24b38d668519205e
mrkmcode <64729649+mrkmcode@users.noreply.github.com>
2020-05-18 00:02:33 +0530

    stopped unlimited jumping

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,6 +7,9 @@ public class PlayerController : MonoBehaviour
     public float moveSpeed;
     public float jumpForce;
     public Rigidbody2D theRB;
+    public Transform groundCheckPoint;
+    public LayerMask whatIsGround;
+    private bool isGrounded;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,8 +21,14 @@ public class PlayerController : MonoBehaviour
     {
         theRB.velocity = new Vector2(moveSpeed * Input.GetAxis("Horizontal"), theRB.velocity.y);
 
+        isGrounded = Physics2D.OverlapCircle(groundCheckPoint.position, .2f, whatIsGround);
+
         if (Input.GetButtonDown("Jump")) {
-            theRB.velocity = new Vector2(theRB.velocity.x, jumpForce);
+
+            if (isGrounded) {
+                theRB.velocity = new Vector2(theRB.velocity.x, jumpForce);
+            }
+        
         }
     }
 }
